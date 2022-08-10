9bf097a884dadbafc3c700cf7b5cc00bf8b1dd56
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2022-08-11 01:14:35 +0200

    feat(): other animated dragon + smoother movement

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,113 @@
+#![warn(clippy::pedantic)]
+
 use bracket_lib::prelude::*;
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 const FRAME_DURATION: f32 = 40.0;
 
+const DRAGON_FRAMES: [u16; 6] = [64, 1, 2, 3, 2, 1];
+
 // |    0,0  1,0  2,0  3,0  4,0
 // |    0,1  1,1  2,1  3,1  4,1
 // |    0,2  1,2  2,2  3,2  4,2
 // y __________________________> x
+
+struct Player {
+    x: i32,
+    y: f32,
+    velocity: f32,
+    frame: usize, // Usize to index arrays
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y: y as f32,
+            velocity: 0.0,
+            frame: 0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set_active_console(1);
+        ctx.cls();
+        ctx.set_fancy(
+            PointF::new(0.0, self.y),
+            1,
+            Degrees::new(0.0),
+            PointF::new(2.0, 2.0),
+            WHITE,
+            NAVY,
+            DRAGON_FRAMES[self.frame],
+        );
+        ctx.set_active_console(0);
+    }
+
+    fn gravity_and_mode(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.1;
+        }
+        self.y += self.velocity;
+        if self.y < 0.0 {
+            self.y = 0.0;
+        }
+
+        self.x += 1;
+        self.frame += 1;
+        self.frame = self.frame % 6; // % is modulus - remainder
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -1.0; // a negative number, so it moves upward, because 0 is the top of the screen
+    }
+}
+
+struct Obstacle {
+    x: i32,
+    gap_y: i32,
+    size: i32,
+}
+
+impl Obstacle {
+    fn new(x: i32) -> Self {
+        let mut random = RandomNumberGenerator::new();
+        Obstacle {
+            x,
+            gap_y: random.range(5, 45),
+            size: i32::max(10, 40),
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
+        // The ground
+        for x in 0..SCREEN_WIDTH {
+            ctx.set(x, SCREEN_HEIGHT - 1, WHITE, WHITE, to_cp437('#'));
+        }
+
+        let screen_x = self.x - player_x;
+        let half_size = self.size / 2;
+
+        // Top wall
+        for y in 0..self.gap_y - half_size {
+            ctx.set(screen_x, y, WHITE, NAVY, 179);
+        }
+
+        // Bottom wall - now leaving room for the ground
+        for y in self.gap_y + half_size..SCREEN_HEIGHT - 1 {
+            ctx.set(screen_x, y, WHITE, NAVY, 179);
+        }
+    }
+
+    fn hit_obstacle(&self, player: &Player) -> bool {
+        let half_size = self.size / 2;
+        let does_x_match = player.x == self.x;
+        let player_above_gap = (player.y as i32) < self.gap_y - half_size;
+        let player_below_gap = (player.y as i32) > self.gap_y + half_size;
+        does_x_match && (player_above_gap || player_below_gap)
+    }
+}
 
 enum GameMode {
     Menu,
@@ -59,9 +159,8 @@ impl State {
         ctx.print(0, 1, "Press ESC to pause.");
         ctx.print(0, 2, &format!("Score {}", self.score));
 
-        let mut random = RandomNumberGenerator::new();
-
         // add new obstacles with a certain percentage
+        let mut random = RandomNumberGenerator::new();
         if self.frame_time as i32 % 50 == 0 && random.range(1, 40) % 10 == 0 {
             self.obstacles
                 .push(Obstacle::new(self.player.x + SCREEN_WIDTH));
@@ -82,7 +181,7 @@ impl State {
         }
 
         for obstacle in &mut self.obstacles {
-            if self.player.y > SCREEN_HEIGHT || obstacle.hit_obstacle(&self.player) {
+            if self.player.y as i32 > SCREEN_HEIGHT || obstacle.hit_obstacle(&self.player) {
                 self.mode = GameMode::End;
             }
         }
@@ -160,81 +259,6 @@ impl GameState for State {
             GameMode::Playing => self.play(ctx),
             GameMode::Pause => self.pause_menu(ctx),
         }
-    }
-}
-
-struct Player {
-    x: i32,
-    y: i32,
-    velocity: f32,
-}
-
-impl Player {
-    fn new(x: i32, y: i32) -> Self {
-        Player {
-            x,
-            y,
-            velocity: 0.0,
-        }
-    }
-
-    fn render(&mut self, ctx: &mut BTerm) {
-        ctx.set(5, self.y, YELLOW, BLACK, to_cp437('@'));
-    }
-
-    fn gravity_and_mode(&mut self) {
-        if self.velocity < 2.0 {
-            self.velocity += 0.2;
-        }
-        self.y += self.velocity as i32;
-        self.x += 1;
-        if self.y < 0 {
-            self.y = 0;
-        }
-    }
-
-    fn flap(&mut self) {
-        self.velocity = -2.0; // a negative number, so it moves upward, because 0 is the top of the screeen
-    }
-}
-
-struct Obstacle {
-    x: i32,
-    gap_y: i32,
-    size: i32,
-}
-
-impl Obstacle {
-    fn new(x: i32) -> Self {
-        let mut random = RandomNumberGenerator::new();
-        Obstacle {
-            x,
-            gap_y: random.range(5, 45),
-            size: i32::max(10, 40),
-        }
-    }
-
-    fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
-        let screen_x = self.x - player_x;
-        let half_size = self.size / 2;
-
-        // draw the top half of the obstacle
-        for y in 0..self.gap_y - half_size {
-            ctx.set(screen_x, y, WHITE, NAVY, 179);
-        }
-
-        // draw the bottom half of the obstacle
-        for y in self.gap_y + half_size..SCREEN_HEIGHT {
-            ctx.set(screen_x, y, WHITE, NAVY, 179);
-        }
-    }
-
-    fn hit_obstacle(&self, player: &Player) -> bool {
-        let half_size = self.size / 2;
-        let does_x_match = player.x == self.x;
-        let player_above_gap = player.y < self.gap_y - half_size;
-        let player_below_gap = player.y > self.gap_y + half_size;
-        does_x_match && (player_above_gap || player_below_gap)
     }
 }
 
